99163438672214042d9271eada225bb1877aab97
Olga <olgakovl96@gmail.com>
2023-07-05 12:45:53 +0300

    HW task 1_4

---
+++ Lesson_1/1_4/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Enter 3 numbers: ");
+
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+int c = int.Parse(Console.ReadLine()!);
+int max = a;
+
+if (b>max)
+{
+    max = b;
+}
+else if (c>max)
+{
+    max = c;
+}
+else
+{
+    max =a;
+}
+
+Console.Write(max);
+
+
