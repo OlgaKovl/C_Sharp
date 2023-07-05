98f2bb09b80cb60cb8b80a45ad82f4b4bf6747fd
Olga <olgakovl96@gmail.com>
2023-07-05 13:35:00 +0300

    HW task 1_8

---
+++ Lesson_1/1_8/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Enter a number: ");
+
+int a = int.Parse(Console.ReadLine()!);
+int count = 2;
+while (count <= a)
+{
+    Console.Write($"{count}");
+    count +=2;
+}
+
+    
+
+
+
+    
