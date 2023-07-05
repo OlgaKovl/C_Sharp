67879c5330fded69a5aa3ee815b89a721fd682ed
Olga <olgakovl96@gmail.com>
2023-07-05 12:01:16 +0300

    Classwork_task 5

---
+++ Lesson_1/1_5/Program.cs
@@ -0,0 +1,11 @@
+﻿
+int a = int.Parse(Console.ReadLine()!); //a = -6
+int count = -a; //count = 6
+Console.WriteLine(count);
+while (count!=a){
+if (a>0)
+count++;//count=count+1
+else
+count--;//count=count-1
+Console.WriteLine(count);
+}
