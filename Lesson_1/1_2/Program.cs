2dc8af62576f7f7cc92939d31eae0c40feaf79bd
Olga <olgakovl96@gmail.com>
2023-07-05 12:27:53 +0300

    HW task 1_2

---
+++ Lesson_1/1_2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Enter a two numbers: ");
+
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+
+if (a>b)
+{
+Console.Write($"max={a} min={b}");
+}
+else
+{
+Console.Write($"max={b} min={a}");   
+}
+
+
+
