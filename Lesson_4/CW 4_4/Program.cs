70bc950809d1a34074a9d0faf9961c054a79cc12
Olga <olgakovl96@gmail.com>
2023-07-12 21:15:06 +0300

    HW 4_1

---
+++ Lesson_4/CW 4_4/Program.cs
@@ -0,0 +1,23 @@
+﻿
+void FillArray(int[] collection)
+{
+for (int i = 0; i < collection.Length; i++)
+{
+collection[i] = new Random().Next(0,2);
+i++;
+}
+}
+void printArray(int[] col)
+{
+int count = col.Length;
+int position = 0;
+while (position < count )
+{
+Console.Write(col[position]+",");
+position++;
+
+}
+}
+int[] array = new int[8];
+FillArray(array);
+printArray(array);
---
+++ Lesson_4/HW 4_1/Program.cs
@@ -0,0 +1,5 @@
+﻿
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine(Math.Pow(a, b));
+
