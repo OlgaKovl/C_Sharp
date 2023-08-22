19308fc607ace3416f02d04c6dc867d8208e50ac
Olga <olgakovl96@gmail.com>
2023-08-22 19:43:18 +0300

    HW_1 done

---
+++ Final work/HW_1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SimpleEvenNumbersRecursion
+{
+    class Program
+    {
+        static void PrintEvenNumbers(int start, int end)
+        {
+            if (start > end)
+            {
+                return;
+            }
+
+            if (start % 2 == 0)
+            {
+                Console.WriteLine(start);
+            }
+
+            PrintEvenNumbers(start + 1, end);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите начальное значение (M): ");
+            int m = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите конечное значение (N): ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Чётные числа от {m} до {n}:");
+            PrintEvenNumbers(m, n);
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+
