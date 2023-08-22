a6e5f382b9f9b9da5835f58a9e0b13f958acada1
Olga <olgakovl96@gmail.com>
2023-08-22 19:47:21 +0300

    HW_2 done

---
+++ Final work/HW_2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SumOfNaturalNumbersRecursion
+{
+    class Program
+    {
+        static int CalculateSum(int start, int end)
+        {
+            if (start > end)
+            {
+                return 0;
+            }
+
+            return start + CalculateSum(start + 1, end);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите начальное значение (M): ");
+            int m = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите конечное значение (N): ");
+            int n = int.Parse(Console.ReadLine());
+
+            int sum = CalculateSum(m, n);
+
+            Console.WriteLine($"Сумма натуральных чисел от {m} до {n}: {sum}");
+
+            Console.ReadLine();
+        }
+    }
+}
